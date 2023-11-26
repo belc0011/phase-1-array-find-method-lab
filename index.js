311664314c2e0046679dc8e7685faf890f85cd22
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-26 10:37:55 -0700

    completed array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+function superbowlWin(array) {
+let winningYear
+const winningObject = array.find(element => element.result === "W");
+if (winningObject) {
+winningYear = winningObject.year;
+}
+return winningYear;
+}
+
+console.log(superbowlWin(record));
